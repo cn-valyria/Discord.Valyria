a87bed765bb80d682785bd9bd0e50b82713c9f97
lilweirdward <zachw38@gmail.com>
2022-04-09 16:34:57 -0400

    Fixed descriptions

--- src/Api/Modules/NationModule.cs
+++ src/Api/Modules/NationModule.cs
@@ -1,28 +1,53 @@
 using Discord.Interactions;
 using Discord.Rest;
+using Entities;
+using Repository.Services.Cybernations;
 
 namespace Api.Modules;
 
 [Group(name: "nation", description: "Commands for fetching data related to a nation in CN")]
 public class NationModule : RestInteractionModuleBase<RestInteractionContext>
 {
+    private readonly INationService _nationService;
+
+    public NationModule(INationService nationService) => _nationService = nationService;
+
     [SlashCommand(name: "search", description: "Attempts to find a single nation given a search parameter. If multiple matches are found then you can follow-up with the correct match")]
     public async Task Search(
         [Summary(name: "search-text", description: "Some text that should tie back to your nation. Currently supports: nation name, ruler name")] string searchText,
         [Summary(name: "nation-id", description: "The ID of the nation you want to find. Helpful if you don't want to deal with fuzzy searching")] int nationId = 0,
-        [Summary(name: "ephemeral", description: "Determines whether the results are only visible to you (true) or visible to everyone in the channel (false). Defaults to false")] bool ephemeral = false
+        [Summary(name: "ephemeral", description: "Determines whether the results are only visible to you (true) or visible to everyone (false)")] bool ephemeral = false
     )
     {
-        
+        var nation = await QueryNation();
+        if (nation is null)
+            return;
+
+        await RespondAsync($"Found nation {nation.Id}, name {nation.Name}, ruler {nation.RulerName}, at {nation.Strength} NS");
+
+        async Task<Nation?> QueryNation()
+        {
+            if (nationId > 0)
+                return await _nationService.GetNationAsync(nationId);
+            else 
+            {
+                var searchResults = await _nationService.SearchNationsAsync(searchText);
+                if (searchResults.Count() == 1)
+                    return searchResults.First();
+
+                await FollowupAsync("Search returned too many nations. Please try again");
+                return null;
+            }
+        }
     }
 
     [SlashCommand(name: "history", description: "Searches all historical entries for a given nation")]
     public async Task History(
         [Summary(name: "nation-id", description: "The NationID of the nation that you want to view history for")] int nationId,
         [Summary(name: "updated-lower-bound", description: "Basically, how far back you want to search for updates to the nation")] DateTime? updatedLowerBound = null,
-        [Summary(name: "updated-upper-bound", description: "When you want the search to stop looking for updates. Generally only useful for searching for updates in a specific date range")] DateTime? updatedUpperBound = null,
-        [Summary(name: "limit", description: "How many historical records you want returned in the results. Note that the more you request, the worse the embed might look")] int limit = 10,
-        [Summary(name: "offset", description: "How many results you want to skip before starting the search. Useful if you want to page through results instead of requesting all of them at once")] int offset = 0
+        [Summary(name: "updated-upper-bound", description: "When you want the search to stop looking for updates")] DateTime? updatedUpperBound = null,
+        [Summary(name: "limit", description: "How many historical records you want returned in the results")] int limit = 10,
+        [Summary(name: "offset", description: "How many results you want to skip before starting the search")] int offset = 0
     )
     {
         await RespondAsync("This command is not implemented yet. Please ping @lilweirdward to finish developing this.");
@@ -31,7 +56,7 @@ public class NationModule : RestInteractionModuleBase<RestInteractionContext>
     [SlashCommand(name: "range", description: "Finds all nations that are in a list of alliances and in range of the provided nation")]
     public async Task NationsInRange(
         [Summary(name: "nation-id", description: "The NationID of the nation that you want to view history for")] int nationId,
-        [Summary(name: "alliance-names", description: "A list of alliance names that you want to search for targets in. The names must exactly match the full name in CN. Multiple names are supported as long as they are separated by commas")] string allianceNames
+        [Summary(name: "alliance-names", description: "A list of alliance names that you want to search for targets in")] string allianceNames
     )
     {
         await RespondAsync("This command is not implemented yet. Please ping @lilweirdward to finish developing this.");
